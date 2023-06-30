1079336f49512fbd5cda27d189269165acc9146d
gusti <dwiputra595@gmail.com>
2023-07-01 00:36:02 +0700

    Revisi Membuat Web menjadi cepat Ketiga

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
+import {astroImageTools} from 'astro-imagetools';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()]
+
+  integrations: [astroImageTools, compress()]
+
 });
